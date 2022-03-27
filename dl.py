d38e8e2672c23cd1a18f5a78cff961e1d67817da
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-27 16:37:29 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,9 +1,7 @@
-import youtube_dl
-import os
-from sample_config import Config
+import youtube_dl 
 
 
 video = {}
-url = "https://www.youtube.com/watch?v=Bb8bnjnEM00"
+url = "https://youtu.be/rruVLXierXU"
 with youtube_dl.YoutubeDL(video) as ydl:
  ydl.download([url])
